c4ddfe35dddc3bbcd79eb92ab676c719a93977d9
unknown <bikerstevefitness@gmail.com>
2018-05-29 14:11:41 -0600

    welcome component configured, fonts added to index.html

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import LargeBtn from './LargeBtn'
+
 export default class Welcome extends Component{
     render(){
         return(
-            <div className="welcome-comp" style={{height: "100vh", width: "100%", backgroundImage: "url('')"}}>
-                <h1 style={{fontSize: "5em"}}><span className="pink-pinetree">Pinetree</span> Boutique Apartments</h1>
+            <div className="welcome-comp">
+            <section className="welcome-head">
+                <h1><span className="pink-pinetree">Pinetree</span> Boutique Apartments</h1>
                 <p>We Create Memories That Last Forever.</p>
                 <LargeBtn styleClass="experience-btn">EXPERIENCE PINETREE</LargeBtn>
+            </section>
                 <div className="upper-right">
                     <p>Switch to</p>
                     <button className="lang-btn">AM</button>
                 </div>
                 <div className="scroll-tag">
+
                     <p>Scroll</p>
-                    {/* <svg /> */}
+                    <p id="arrow">↓</p>
                 </div>
             </div>
         )
