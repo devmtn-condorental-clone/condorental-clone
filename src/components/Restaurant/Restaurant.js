49b5f3bd1e80059ac602db44a640f436846a7824
Ricardo Carosa <ricardocarosa@Ricardos-MacBook-Pro.local>
2018-05-29 14:33:49 -0600

    amazing with one pic

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import '../../style/style.css';
+import '../../style/restaurant.css';
+import steak from '../../style/images/amazing_restaurant_steak.jpg'
 
 class Restaurant extends Component {
 
@@ -8,14 +9,17 @@ class Restaurant extends Component {
     return(
         <section className = 'Restaurant'>
             <div className = 'amazing_container'>
-                <h3 className = 'title_amazing'> Amazing Restaurant </h3>
-                <div className = 'text_amazing'>
-                    <p>
-                        Seeking out new flavours and creative cuisine is one of the best things about any holiday. Created from the great passion of the owner’s love of fresh and home grown food, the cuisine of Boutique Pine Tree combines traditional and contemporary dishes. We strive to provide a unique dining experience with exceptional service.
-                    </p>
-                    <p> Read more </p>
+                <div className = 'amazing_line'>
                 </div>
+                    <h3 className = 'title_amazing'> Amazing Restaurant </h3>
+                    <div className = 'text_amazing'>
+                        <p>
+                            Seeking out new flavours and creative cuisine is one of the best things about any <br/> holiday. Created from the great passion of the owner’s love of fresh and home <br/> grown food, the cuisine of Boutique Pine Tree combines traditional and <br/> contemporary dishes. We strive to provide a unique dining experience with <br/> exceptional service.
+                        </p>
+                        <p className = 'amazing_read_more'> Read more </p>
+                    </div>
             </div>
+            <span> <img className = 'amazing_steak' src={steak} alt="steak"/> </span> 
         </section>
     )
     }
