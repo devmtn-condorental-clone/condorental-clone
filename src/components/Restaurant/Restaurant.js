fdce7aa310b54a3c253296ac886bc6e15c422b0d
Ricardo Carosa <ricardocarosa@Ricardos-MacBook-Pro.local>
2018-05-29 10:06:03 -0600

    fixed rest.js

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import '../../style/style.css';
 
 class Restaurant extends Component {
     constructor(){
@@ -12,9 +12,17 @@ class Restaurant extends Component {
 
     render(){
     return(
-        <div>
-            Resturaunt
-        </div>
+        <section className = 'Restaurant'>
+            <div className = 'amazing_container'>
+                <h3 className = 'title_amazing'> Amazing Restaurant </h3>
+                <div className = 'text_amazing'>
+                    <p>
+                        Seeking out new flavours and creative cuisine is one of the best things about any holiday. Created from the great passion of the owner’s love of fresh and home grown food, the cuisine of Boutique Pine Tree combines traditional and contemporary dishes. We strive to provide a unique dining experience with exceptional service.
+                    </p>
+                    <p> Read more </p>
+                </div>
+            </div>
+        </section>
     )
     }
 }
