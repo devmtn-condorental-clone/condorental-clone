3c6a3f7d932f841d4a20d5f36c05ebad7af65df4
Dev-Zander <nx2reign@gmail.com>
2018-05-29 22:32:18 -0600

    finshed header minus ham menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,72 @@
 import React, {Component} from 'react';
 import '../style/header.css';
-import LOGO from '../style/images/logo_2x.png';
+import Logo from '../style/images/logo_2x.png';
+import FaceBookLite from '../style/images/facebook-light.svg';
+import FaceBookDark from '../style/images/facebook-dark.svg';
+import InstaLight from '../style/images/instagram-light.svg';
+import InstaDark from '../style/images/instagram-dark.svg';
+import TripLight from '../style/images/tripadvisor-light.svg';
+import TripDark from '../style/images/tripadvisor-dark.svg';
+
 
 
 
 
 class Header extends Component{
+    constructor(){
+        super()
+        this.state = {
+            facebook: FaceBookLite,
+            instagram: InstaLight,
+            tripadvisor: TripLight
+        }
+    }
     render(){
         return(
 
             <div className="header-main-container">
-            <div className="header-logo-container"><img src={LOGO} className="header-logo" alt="Logo"/>
-            <div className="header-main-text">MENU</div>
-            </div>
+            <div className="header-logo-container"><img src={Logo} className="header-logo" alt="Logo"/>
             
-            <div className="header-social-container"></div>
+            </div>
+            <div className="header-main-text">MENU</div>
+            
+            <div className="header-social-container">
+            <div><img className="header-social-icon-fb" 
+            src={this.state.facebook}
+            onMouseEnter={()=>{
+                this.setState({
+                    facebook:FaceBookDark
+                })
+            }}
+            onMouseOut={()=>{
+                this.setState({
+                    facebook:FaceBookLite
+                })
+            }}
+            alt="facebook-logo"/></div> 
+            <div><img className="header-social-icon-ig"   src={this.state.instagram}
+            onMouseEnter={()=>{
+                this.setState({
+                    instagram:InstaDark
+                })
+            }}
+            onMouseOut={()=>{
+                this.setState({
+                    instagram:InstaLight
+                })
+            }} alt="insta-logo"/></div>
+            <div><img className="header-social-icon-ta"   src={this.state.tripadvisor}
+            onMouseEnter={()=>{
+                this.setState({
+                    tripadvisor:TripDark
+                })
+            }}
+            onMouseOut={()=>{
+                this.setState({
+                    tripadvisor:TripLight
+                })
+            }} alt="tripadvisor-logo"/></div>
+            </div>
 
             </div>
         )
