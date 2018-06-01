533121011d2ccd4536f657cbf985aa7354930af7
unknown <bikerstevefitness@gmail.com>
2018-05-31 22:03:34 -0600

    broken parallax

--- src/components/Paradise.js
+++ src/components/Paradise.js
@@ -8,18 +8,20 @@ import bgImg from '../style/images/paradise_background_p.png'
 class Paradise extends Component{
     render() {
         return(
-            <section className="paradise-comp">
-                <img className="pool-img" src={leftimg} alt="Pool View"/>
-                <div className="left-shadow shadow"/>
-                <div className="right-shadow shadow"/>
-                <img className="building-img" src={centerImg} alt="Building View From Water"/>
-                <img className="steak-img" src={parSteak} alt="Juicy Garnished Steak"/>
-                <img className="bg-img" src={bgImg} alt="Pine Tree Watermark"/>
-                <section className="paradise-content">
-                    <h2>Paradise between land and sea</h2>
-                    <p>Boutique Pine Tree Resort is a charming 4 star boutique hotel that is located in the lush village of Saplunara on the Island of Mljet.</p>
-                    <br />
-                    <p>Suspended between land and sea, Boutique Pine Tree Resort is an Adriatic style villa offering an intimate location, fresh white décor but with just the right touch of colour to enhance its lush surroundings! The large windows and spacious terraces allow you to experience the remarkable colours of the scenery which change from sunrise to sunset.</p>
+            <section className="parallax_paradise_cont">
+                <section className="paradise-comp">
+                    <img className="pool-img" src={leftimg} alt="Pool View"/>
+                    <div className="left-shadow shadow"/>
+                    <div className="right-shadow shadow"/>
+                    <img className="building-img" src={centerImg} alt="Building View From Water"/>
+                    <img className="steak-img" src={parSteak} alt="Juicy Garnished Steak"/>
+                    <img className="bg-img" src={bgImg} alt="Pine Tree Watermark"/>
+                    <section className="paradise-content">
+                        <h2>Paradise between land and sea</h2>
+                        <p>Boutique Pine Tree Resort is a charming 4 star boutique hotel that is located in the lush village of Saplunara on the Island of Mljet.</p>
+                        <br />
+                        <p>Suspended between land and sea, Boutique Pine Tree Resort is an Adriatic style villa offering an intimate location, fresh white décor but with just the right touch of colour to enhance its lush surroundings! The large windows and spacious terraces allow you to experience the remarkable colours of the scenery which change from sunrise to sunset.</p>
+                    </section>
                 </section>
             </section>
         )
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,45 +2,22 @@ import React, { Component } from 'react'
 import LargeBtn from './LargeBtn'
 
 export default class Welcome extends Component{
-    // constructor(){
-    //     super()
-    //     this.state = {
-    //         welcomeClasses: '',
-    //         amBtnClass: ''
-    //     }
-    // }
-    // componentDidMount(){
-    //     this.setState({
-    //         welcomeClasses: 'shown'
-    //     })
-    // }
-
-    // componentDidUpdate(){
-    //     const { welcomeClasses, amBtnClass } = this.state
-    //     if(welcomeClasses === 'shown' && amBtnClass === ''){
-    //         this.setState({
-    //             amBtnClass: 'comeDown'
-    //         })
-    //     }
-    // }
-    
     render(){
         return(
             <div className="welcome-comp">
-                <section className="welcome-head">
-                    <h1 ><span className="pink-pinetree">Pinetree</span> Boutique Apartments</h1>
-                    <p >We Create Memories That Last Forever.</p>
-                    <LargeBtn styleClass="experience-btn">EXPERIENCE PINETREE</LargeBtn>
-                </section>
-                <div className={`upper-right`}>
-                    <p>Switch to</p>
-                    <button className="lang-btn">AM</button>
-                </div>
-                <div className="scroll-tag">
-
-                    <p>Scroll</p>
-                    <p id="arrow">↓</p>
-                </div>
+                    <section className="welcome-head">
+                        <h1 ><span className="pink-pinetree">Pinetree</span> Boutique Apartments</h1>
+                        <p >We Create Memories That Last Forever.</p>
+                        <LargeBtn styleClass="experience-btn">EXPERIENCE PINETREE</LargeBtn>
+                    </section>
+                    <div className="upper-right">
+                        <p>Switch to</p>
+                        <button className="lang-btn">AM</button>
+                    </div>
+                    <div className="scroll-tag">
+                        <p>Scroll</p>
+                        <p id="arrow">↓</p>
+                    </div>
             </div>
         )
     }
