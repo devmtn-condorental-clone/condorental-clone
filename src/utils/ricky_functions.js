0bbe92118a403acde073e092f40a82c2b3996bd6
unknown <bikerstevefitness@gmail.com>
2018-06-12 23:13:11 -0600

    modal transition fixing and hooking up guest popover

--- src/utils/ricky_functions.js
+++ src/utils/ricky_functions.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+var axios = require('axios')
 
 module.exports = {
     getAllCondos: () => axios.get('http://localhost:7373/api/condos').then(res => res.data).catch(err => 'error')
