d947b161e4fe8f0850273c20278b58367bb9fdc9
Dev-Zander <nx2reign@gmail.com>
2018-06-14 21:44:22 -0600

    added test

---
+++ cypress/integration/examples/donny_project_tests.js
@@ -0,0 +1,5 @@
+// describe("Test One", () => {
+//     it('Group-Project', () => {
+//         cy.visit('http://localhost:3000/#/')
+//     })
+// })
---
+++ src/utils/don_functions.js
@@ -0,0 +1,9 @@
+module.exports ={
+    handleAddPersonInf(infants){
+        if(infants <= 1){
+          let currentInf = infants
+          return currentInf + 1
+        }
+      }
+}
+
